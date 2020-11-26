b48c90b927875b2a7a17093937816b175a1dcb78
Bruno e Vanessa <vanessaf689@gmail.com>
2020-11-26 14:08:53 -0300

    progresso na visualizacao do tempo

--- src/app/pages/home/components/current-weather/current-weather.component.ts
+++ src/app/pages/home/components/current-weather/current-weather.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, Output } from '@angular/core';
 import { CityWeather } from 'src/app/shared/models/weather.model';
 
 @Component({
   selector: 'jv-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.scss']
+  styleUrls: ['./current-weather.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CurrentWeatherComponent {
 
--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 
 import { LoaderComponent } from './loader/loader.component';
+import { DetalidWeatherComponent } from './detalid-weather/detalid-weather.component';
 
 @NgModule({
   imports: [
@@ -9,6 +10,7 @@ import { LoaderComponent } from './loader/loader.component';
   ],
   declarations: [
     LoaderComponent,
+    DetalidWeatherComponent,
   ],
 })
 export class ComponentsModule {
